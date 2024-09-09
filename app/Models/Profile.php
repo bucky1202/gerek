cddc632150d0cfd1fe29f2e6a36bad008ae0e806
bucky1202 <bakyegenov@gmail.com>
2024-09-09 18:41:37 +0500

    created relationships

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'fullname',
+        'date_of_birth',
+        'address',
+        'avatar',
+        'job_title',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
